6f13e0d1285ca5d868c0af75d3eb7c0707fe9aa9
manasvi9745 <manasvisingh1894@gmail.com>
2026-02-22 20:56:43 +0530

    Add solution for minimum number of arrows to burst balloons in cpp
    Implemented a solution to find the minimum number of arrows required to burst all balloons based on their coordinates.

---
+++ Greedy/Q5. Minimum Number of Arrows to Burst Balloons/manasvi9745.cpp
@@ -0,0 +1,29 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int findMinArrowShots(vector<vector<int>>& points) {
+        
+        if (points.empty()) return 0;
+        
+        // Sort by ending coordinate
+        sort(points.begin(), points.end(), 
+             [](const vector<int>& a, const vector<int>& b) {
+                 return (long long)a[1] < (long long)b[1];
+             });
+        
+        int arrows = 1;
+        long long arrowPos = points[0][1];
+        
+        for (int i = 1; i < points.size(); i++) {
+            if (points[i][0] > arrowPos) {
+                arrows++;
+                arrowPos = points[i][1];
+            }
+        }
+        
+        return arrows;
+    }
+};
