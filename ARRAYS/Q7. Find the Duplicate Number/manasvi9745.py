a75f18d2c66f968ee2fc7aae8f6698bcbd19914f
manasvi9745 <manasvisingh1894@gmail.com>
2026-02-22 21:16:06 +0530

    Add solution to find duplicate number in array
    Implement Floyd's Tortoise and Hare algorithm to find the duplicate number in a list.

---
+++ ARRAYS/Q7. Find the Duplicate Number/manasvi9745.py
@@ -0,0 +1,23 @@
+from typing import List
+
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        
+        # Phase 1: Find intersection point
+        slow = nums[0]
+        fast = nums[0]
+        
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if slow == fast:
+                break
+        
+        # Phase 2: Find entrance to cycle
+        slow = nums[0]
+        
+        while slow != fast:
+            slow = nums[slow]
+            fast = nums[fast]
+        
+        return slow
