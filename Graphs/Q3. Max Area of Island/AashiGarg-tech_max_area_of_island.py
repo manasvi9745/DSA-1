3d9b3b2939e4919b6731bc93b32b8acdd8f26d3a
Aashi Garg <aashigarg2504@gmail.com>
2026-02-22 17:03:19 +0530

    Graphs - maximum area of island - python

---
+++ Graphs/Q3. Max Area of Island/AashiGarg-tech_max_area_of_island.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def maxAreaOfIsland(self, grid):
+        m, n = len(grid), len(grid[0])
+
+        def dfs(r, c):
+            if r < 0 or r >= m or c < 0 or c >= n or grid[r][c] == 0:
+                return 0
+
+            grid[r][c] = 0 
+            area = 1
+
+            area += dfs(r+1, c)
+            area += dfs(r-1, c)
+            area += dfs(r, c+1)
+            area += dfs(r, c-1)
+
+            return area
+
+        max_area = 0
+
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1:
+                    max_area = max(max_area, dfs(i, j))
+
+        return max_area
